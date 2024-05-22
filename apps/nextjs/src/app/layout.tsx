e10300ba432eb3fccd3017b46a915f39bdda443c
Ong Hin Journ <onghinjourn@gmail.com>
2024-05-22 22:39:56 +0800

    feat: :sparkles: adds navigation

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -35,18 +35,6 @@ const RootLayout = ({
           enableSystem
           disableTransitionOnChange
         >
-          <header>
-            <nav>
-              <ul>
-                <li>
-                  <a href="/">Home</a>
-                </li>
-                <li>
-                  <a href="/about">About</a>
-                </li>
-              </ul>
-            </nav>
-          </header>
           {children}
         </ThemeProvider>
       </body>
--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,5 +1,51 @@
+'use client';
+
+import Link from 'next/link';
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <header className="sticky bottom-0 left-0 z-50 w-full">
+      <div className="container flex h-14 max-w-screen-2xl justify-center items-center">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem className="m-0">
+              <Link href="/" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Leaderboard
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem className="m-0">
+              <Link href="/rules" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Rules
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem className="m-0">
+              <Link href={'a'} legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Qualifier Booking
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </header>
+  );
 };
 
 export default Home;
